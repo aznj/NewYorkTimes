d92ae79a7c95a90f27a66602dcbf4b43d01f8a89
Azlan Jamal <azlan@newsoft.com.my>
2021-10-29 15:09:22 +0800

    Remove unused code.

--- app/src/main/java/dev/aznj/newyorktimes/compose/Theme.kt
+++ app/src/main/java/dev/aznj/newyorktimes/compose/Theme.kt
@@ -19,14 +19,6 @@ private val LightColorPalette = lightColors(
     primary = LighterGrey,
     primaryVariant = FogGrey,
     secondary = Blue
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
